f8af07fb46f2ebabd50691a06b8fb857bbb7d3c0
LyoshaX0f <lyoshax0f@yandex.ru>
2022-07-11 20:19:41 +0300

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener ('DOMContentLoaded', function() {
+  document.querySelectorAll('.working__items-btn') .forEach(function(workingItemsBtn) {
+   workingItemsBtn.addEventListener('click', function(event) {
+      const path = event.currentTarget.dataset.path
+    })
+
+})
